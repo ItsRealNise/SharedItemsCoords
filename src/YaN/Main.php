f0d37b99e11a17a5205e49409db8572f2119ca1c
ItsRealNise <90197402+ItsRealNise@users.noreply.github.com>
2021-09-15 11:21:02 +0700

    Update Main.php

--- src/YaN/Main.php
+++ src/YaN/Main.php
@@ -27,7 +27,7 @@ class Main extends PluginBase implements Listener{
         $item = $p->getInventory()->getItemInHand()->getName();
         $cfg = $this->config->get("item-msg");
         $cfg = str_replace(["{name}", "{item}"], [$p->getName(), $item], $cfg);
-        ms = str_replace("[i]", $cfg, $msg);
+        $ms = str_replace("[i]", $cfg, $msg);
         $e->setMessage($ms);
       }
     }
